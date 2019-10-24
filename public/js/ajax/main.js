76090c3d104b13a3605355b81b8fc580bfb5fd1e
Godwin Nicholas <iamgodwin@gmail.com>
2019-10-24 17:14:58 +0100

    fixed ajax request

--- public/js/ajax/main.js
+++ public/js/ajax/main.js
@@ -2,7 +2,7 @@
 const printBtn = document.querySelector("#onePrint");
 
 async function postBet(players, stake, payWin, cb) {
-    const response = await fetch(`http://enaland.com/cornerBet/ticket/${players}/${stake}/${payWin}/${new Date()}`);
+    const response = await fetch(`https://enaland.com/cornerBet/ticket/${players}/${stake}/${payWin}/${new Date()}`);
     const bet = await response.json();
     await printBtn.setAttribute("p_id", bet._id)
     printBtn.removeAttribute("disabled");
