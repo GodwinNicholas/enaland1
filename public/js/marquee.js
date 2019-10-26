b3de202ff607431c786a4033961abfabdc96352d
Godwin Nicholas <iamgodwin@gmail.com>
2019-10-26 14:49:02 +0100

    fresh start

--- public/js/marquee.js
+++ public/js/marquee.js
@@ -2,22 +2,9 @@ const week = document.querySelector("weekHolder");
 
 function marquee(winner) {
 
-    if (winner == 404) {
-        const final = `
-        <marquee class="marquee">
-            <p class="text-success"> There are no winners <span class="text-warning">(N)${winner.potWin}</span>:
-            ${winner.name.toUpperCase()}.
-            </p>
-        </marquee>
-        `;
-
-        document.querySelector(".footer").innerHTML = final;
-        return;
-    }
-
     const final = `
     <marquee class="marquee">
-        <p class="text-success">Congratulations to the winner of week ${week.innerText} <span class="text-warning">(N)${winner.potWin}</span>:
+        <p class="text-success">Congratulations to the winner of week ${week} <span class="text-warning">(N)${winner.potWin}</span>:
         ${winner.name.toUpperCase()}.
         </p>
     </marquee>
